585b14cafa31c3e562bcfba5e64234be37fc6f3b
Anatoly Wein <anatoly.wein@gmail.com>
2026-02-19 12:14:32 +0100

    add deploy script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+
+# Generate client
+sudo cp -r fa-quiz-client/dist/* /var/www/fa-quiz-client/
+
+# Generate server binary
+cd fa-quiz-api
+go build -o fa-quiz-api src/main.go
+cd ..
+
+# Update client
+sudo systemctl reload caddy
+
+# Update server
+sudo systemctl reload fa-quiz-api
